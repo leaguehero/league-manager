ee3080e47ffbfedfb1e7db58fe34ff7dbbb0e28b
Rob Schwartz <robertschwar@gmail.com>
2015-12-11 11:52:45 -0500

    added routes for all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :league
+  resources :leagues
+  resources :teams
+  resources :games
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
