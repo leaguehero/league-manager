aef34ee5f3d41535b717e9d3f445b1d3b324e394
Rob Schwartz <robertschwar@gmail.com>
2015-12-12 17:30:29 -0500

    added authenticate_user! to view index of league page

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,4 +1,8 @@
 class LeaguesController < ApplicationController
+
+# can use except or only to specify pages
+  before_action :authenticate_user!
+
   def index
     @league = "Rob's Rockin Robin"
   end
