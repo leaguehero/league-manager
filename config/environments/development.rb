d173940bbee1378b111369afaa06385b24937f16
Rob Schwartz <robertschwar@gmail.com>
2015-12-13 11:42:03 -0500

    more email confirm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # to send out mailers in dev mode
-  config.action_mailer.default_url_options = { :host=> 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 end
