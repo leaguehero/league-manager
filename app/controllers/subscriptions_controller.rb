60b99ff72da38ea3ee098b8f43efbbd50f149af4
Rob Schwartz <robertschwar@gmail.com>
2015-12-13 20:25:49 -0500

    added plan drop down and stripe api call in subscriptions#create

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,23 @@
 class SubscriptionsController < ApplicationController
   def new
-
+    @plans = Plan.all
   end
   def create
     ap "Inside Create Payment"
     ap params
+
+    # Get the credit card details submitted by the form
+    token = params[:stripeToken]
+    plan  = params[:plan][:stripe_id]
+    email = current_user.email
+    # Create a Customer
+    customer = Stripe::Customer.create(
+      :source => token,
+      :plan => plan,
+      :email => email
+    )
+
+  rescue => e
+    redirect_to :new_subscription, :flash => {:error => e.message}
   end
 end
