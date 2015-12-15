e77befa4b83155c5fc0aff75fceff2b415dd72f0
Justin Seidl <justin@donde.io>
2015-12-15 17:26:32 -0600

    LH :: separate market layout from application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,23 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # Change layout marketing/application
+  layout :define_layout
+
+  def define_layout
+    # if devise_controller? && resource_name == :user
+    #   "devise"
+    if params["controller"] == "pages"
+      "market"
+    else
+      "application"
+    end
+  end
+
   protected
 
- def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :subdomain
- end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :subdomain
+  end
 
 end
