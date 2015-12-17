1aa8619becdc1b1485abc03e7320ce70366b5513
Rob Schwartz <robertschwar@gmail.com>
2015-12-16 21:01:48 -0500

    added printing of errors

--- lib/rescue_apartment_middleware.rb
+++ lib/rescue_apartment_middleware.rb
@@ -5,6 +5,8 @@ module RescueApartmentMiddleWare
       # calls Apartment middleware first
       super
     rescue Apartment::TenantNotFound
+      ap "Apartment::TenantNotFound -> Exception occured"
+      ap e.message
       return [404, {"Content-Type" => "text/html"}, ["#{File.read(Rails.root.to_s+ '/public/404.html')}"]]
     end
   end
