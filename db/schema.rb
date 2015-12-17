122dab71f417dafb77e63b2de0b0a6d142ca3ae3
Rob Schwartz <robertschwar@gmail.com>
2015-12-17 08:53:06 -0500

    added league model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216135738) do
+ActiveRecord::Schema.define(version: 20151217133050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20151216135738) do
     t.string   "customer_id"
     t.datetime "active_until"
     t.string   "subdomain"
+  end
+
+  create_table "leagues", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.integer  "max_teams"
+    t.integer  "max_players_per_team"
+    t.string   "admin"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "plans", force: :cascade do |t|
