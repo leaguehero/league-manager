ea775eefb1e0f91ec58a3b5207d5451c7eac31f3
Rob Schwartz <robertschwar@gmail.com>
2015-12-21 22:04:08 -0600

    setting up form to accept params. Added check to make sure subdomain is not taken

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -17,13 +17,24 @@ class LeaguesController < ApplicationController
   end
 
   def create
-    @league = League.create(league_params)
-
-    if @league.save
-      redirect_to root_path, notice: "Thank you for signing up!"
+    @league_subdomain = League.where(subdomain: params["league"]["subdomain"])
+    if @league_subdomain.length > 0
+      flash[:alert] = "subdomain is already taken"
+      redirect_to "new"
     else
-      render "/new"
+    # go to user sign_up page with league params
+    redirect_to new_user_registration_path
     end
+
+
+
+    # @league = League.create(league_params)
+    #
+    # if @league.save
+    #   redirect_to root_path, notice: "Thank you for signing up!"
+    # else
+    #   render "/new"
+    # end
   end
 
   def confirmation
@@ -38,6 +49,6 @@ class LeaguesController < ApplicationController
 
 # Need to add permitted params for Rails 4
   def league_params
-    params.require(:league).permit(:name, :url, :league_name, :max_teams, :max_players_per_team, :admin)
+    params.require(:league).permit(:name, :subdomain, :url, :league_name, :max_teams, :max_players_per_team, :admin)
   end
 end
