09fc19d4d6488bef857f2bc4b4e3f06cd350dabc
Rob Schwartz <robertschwar@gmail.com>
2015-12-22 21:47:57 -0600

    form 3 step process is now set up. Need to pass in params though form to save after stripe success

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,17 +8,20 @@ class ChargesController < ApplicationController
 
     # Get the credit card details submitted by the form
     token = params[:stripeToken]
-
+    league = "entered league name"
+    @amount  = (10 * 10) #(params[:max_teams] * params[:max_players_per_team])
+    @amount_in_cents = @amount * 100
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        :amount => 1000,  # Amount in cents, this will be (max number of teams * max players per team) * 100
+        :amount => @amount_in_cents,
         :currency => "usd",
         :source => token,
-        :description => "Example charge"
+        :description => "Charge for #{league}"
       )
     rescue Stripe::CardError => e
       # The card has been declined
     end
+    @league = League.find(1)
   end
 end
