92a6e0c94f633b9d79580f481ef42b2a07854c9e
Rob Schwartz <robertschwar@gmail.com>
2015-12-28 22:04:47 -0600

    undo fix, diff issue

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,14 +1,17 @@
 class League < ActiveRecord::Base
 
-#   before_create :create_tenant
-#
-# private
-#
-#    def create_tenant
-#      return false unless self.errors.empty?
-#       #  create subdomain
-#       Apartment::Tenant.create(subdomain)
-#       #  switch to subdomain schema right away
-#        Apartment::Tenant.switch!(subdomain)
-#    end
+  validate :subdomain_is_unique, on: :create
+
+  before_create :create_tenant
+
+
+private
+
+   def create_tenant
+     return false unless self.errors.empty?
+      #  create subdomain
+       Apartment::Tenant.create(subdomain)
+      #  switch to subdomain schema right away
+       Apartment::Tenant.switch!(subdomain)
+   end
 end
