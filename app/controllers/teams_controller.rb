fd5e1eef0073cc8a8abb5670ddaf8d3e50ca9174
Rob Schwartz <robertschwar@gmail.com>
2016-01-05 07:54:55 -0600

    added new team and new player views and forms. not finished yet.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,6 +6,35 @@ class TeamsController < ApplicationController
   end
 
   def new
+    @team = Team.new
+  end
 
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+    # go to add players page
+      redirect_to new_player_path(:team_id => @team.id)
+    else
+      redirect_to :back, :flash => {:error => @team.errors.full_messages.join(", ")}
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+# Need to add permitted params for Rails 4
+  def team_params
+    params.require(:team).permit(:name, :captain, :asst_captain)
   end
 end
