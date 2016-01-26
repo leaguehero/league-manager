34c5f858b5b6aebd549c447b5a1e3d1fbbd55363
Rob Schwartz <robertschwar@gmail.com>
2016-01-25 21:03:18 -0600

    started to add raking table

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,29 @@
 module PagesHelper
+  def create_rankings
+    @wins = {}
+    @losses = {}
+    # go through each game and add up how many wins and losses each team has.
+    if Game.all.length > 0
+      Game.all.each do |game|
+        # add to wins hash
+        if game.winner.nil?
+          next
+        else
+          @wins[game.winner] ? @wins[game.winner] += 1 : @wins[game.winner] = 1
+        end
+        # losses
+        if game.loser.nil?
+          next
+        else
+          @losses[game.loser] ? @losses[game.loser] += 1 : @losses[game.loser] = 1
+        end
+      end
+    else
+      # if no games have been made, default all teams to 0 wins
+      Team.all.each do |team|
+        @wins[team.id] = 0
+        @losses[team.id] = 0
+      end
+    end
+  end
 end
