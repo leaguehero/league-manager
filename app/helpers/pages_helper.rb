da74c91d59d228314f1782c3a7441dfc50bdef8f
Rob Schwartz <robertschwar@gmail.com>
2016-01-26 07:27:19 -0600

    created league rankings method

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,29 +1,11 @@
 module PagesHelper
   def create_rankings
-    @wins = {}
-    @losses = {}
-    # go through each game and add up how many wins and losses each team has.
-    if Game.all.length > 0
-      Game.all.each do |game|
-        # add to wins hash
-        if game.winner.nil?
-          next
-        else
-          @wins[game.winner] ? @wins[game.winner] += 1 : @wins[game.winner] = 1
-        end
-        # losses
-        if game.loser.nil?
-          next
-        else
-          @losses[game.loser] ? @losses[game.loser] += 1 : @losses[game.loser] = 1
-        end
-      end
-    else
-      # if no games have been made, default all teams to 0 wins
-      Team.all.each do |team|
-        @wins[team.id] = 0
-        @losses[team.id] = 0
-      end
+    @rankings = {}
+    Team.all.each do |team|
+      @rankings[team.name] = {}
+      @rankings[team.name]["wins"] = Game.where(winner: team.id).count
+      @rankings[team.name]["losses"] = Game.where(loser: team.id).count
     end
+    @rankings = @rankings.sort_by{|k,v| v["wins"]}.reverse
   end
 end
