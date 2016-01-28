177bf02305c9503cb0060be358f70561a2073873
Rob Schwartz <robertschwar@gmail.com>
2016-01-27 20:12:49 -0600

    pushing up for debugging

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,10 +5,11 @@ class ChargesController < ApplicationController
 
   def create
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-
+    ap Stripe.api_key
     pl = PreLeague.find(current_user.pre_league_id)
     # Get the credit card details submitted by the form
     token = params[:stripeToken]
+    ap token
     league = pl["league_name"]
     @amount  = (pl[:max_teams] * pl[:max_players_per_team])
     @amount_in_cents = @amount * 100
