7548d86e57eef13ab052c707a9cb4f1bd19f6203
Justin Seidl <aceeightofspades@gmail.com>
2016-02-01 21:09:52 -0600

    Style progress

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources  :teams
   get "games/generator_options" => "games#generator_options"
   post "games/generate" => "games#generate_games"
-  resources  :games
+  resources  :games, :path => "schedule"
   resources  :players
   get  'charges/confirmation' => "charges#confirmation"
   resources  :charges
