5e324ef3e5ac8605cd049a23180fc53854304fd3
Rob Schwartz <robertschwar@gmail.com>
2016-02-10 20:15:27 -0600

    added league editing ability, only name admin name and admin email for now

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -3,26 +3,20 @@ class LeaguesController < ApplicationController
 # can use except or only to specify pages
   before_action :authenticate_user!
 
-  def index
-    @league = "Rob's Rockin Robin"
-  end
-
-  def new
-    @league = League.new
-  end
-
   def edit
-    @league = League.find(params[:id])
+
   end
 
+  respond_to :html, :json
   def update
-
+    @league.update_attributes(league_params)
+    respond_with @league
   end
 
   private
 
 # Need to add permitted params for Rails 4
   def league_params
-    params.require(:league).permit(:name, :subdomain, :url, :league_name, :max_teams, :max_players_per_team, :admin)
+    params.require(:league).permit(:name, :subdomain, :url, :max_teams, :max_players_per_team, :admin_name, :admin_email)
   end
 end
