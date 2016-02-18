cb3be7a24b7895c3fe526df9ada4cf792d192884
Rob Schwartz <robertschwar@gmail.com>
2016-02-17 21:10:04 -0600

    added welcome post on league creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,13 @@ class PagesController < ApplicationController
     if !request.subdomain.blank?
       create_rankings
       @posts = Post.all
+      if @posts.blank?
+        Post.create(
+        title: "Welcome to League Hero",
+        body: "Congrats on joining the the fastest, simplest way to set up and manage any sports league! If you have any questions or comments along the way, feel free to reach out to your league admin or getleaguehero@gmail.com.
+        Game On!"
+        )
+      end
       # Apartment::Tenant.switch!(request.subdomain)
     end
     if valid_page?
