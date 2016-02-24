d60e8d71c84ada4300d0f26a1339197277569665
Justin Seidl <aceeightofspades@gmail.com>
2016-02-23 19:27:25 -0600

    LH :: add additional subdomain exclusions

--- config/initializers/subdomain_exclusions.rb
+++ config/initializers/subdomain_exclusions.rb
@@ -5,7 +5,8 @@ sub4 = %w(signup gettingstarted home invoice invoices ios ipad iphone log logs m
 sub5 = %w(redirect random public registration resolver rss sandbox search server servers service signin signup sitemap sitenews sites sms sorry ssl staging development)
 sub6 = %w(stats statistics graph graphs survey surveys talk trac git svn translate upload uploads video validation validations email webmaster ww wwww www1 www2 feeds)
 sub7 = %w(secure demo i img img1 img2 img3 css1 css2 css3 js js1 js2)
+sub8 = %w(league leagues club clubs directory find)
 
-subdomains = sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7
+subdomains = sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8
 
 Apartment::Elevators::Subdomain.excluded_subdomains = subdomains
