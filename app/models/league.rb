95a09f1f7f2b517f8091578aa891246973cb7f0b
Justin Seidl <aceeightofspades@gmail.com>
2016-02-24 10:26:15 -0600

    Add 30 day free trial message to charges, update default league image

--- app/models/league.rb
+++ app/models/league.rb
@@ -7,7 +7,7 @@ class League < ActiveRecord::Base
 # paperclip attachments for images
   has_attached_file :avatar,
                     styles: { medium: "300x300>", thumb: "100x100>" },
-                    default_url: "https://s3.amazonaws.com/leaguehero/image-sports.png",
+                    default_url: "https://s3.amazonaws.com/leaguehero/avatar-lh-blank.png",
                     :storage => :s3
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates_attachment_size :avatar, :less_than => 0.2.megabytes
