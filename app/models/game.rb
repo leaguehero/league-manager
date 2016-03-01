3a983bfeee86ea245a6c3778f7e34265dd595c44
Rob Schwartz <robertschwar@gmail.com>
2016-02-29 20:11:48 -0600

    Fixed game edit bug where it would default to 6:00AM

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
   before_save :convert_time
 
   def convert_time
-    self.time = Time.parse(self.time).strftime("%I:%M %p")
+    self.time = Time.parse(self.time).strftime("%l:%M%p")
   end
 end
