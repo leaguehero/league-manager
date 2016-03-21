6b4c7e7682e4cc3e3df47ddfa4e5f7ede27285cd
Rob Schwartz <robertschwar@gmail.com>
2016-03-21 11:49:22 -0500

    use win percentage instead of wins for rankings

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,12 +4,15 @@ module PagesHelper
     Team.all.each do |team|
       @rankings[team.name] = {}
       @rankings[team.name]["id"] = team.id
-      @rankings[team.name]["wins"] = Game.where(winner: team.id).count
+      wins = Game.where(winner: team.id).count
+      @rankings[team.name]["wins"] = wins
       @rankings[team.name]["losses"] = Game.where(loser: team.id).count
+      game_count = (Game.where(winner: team.id).count + Game.where(loser: team.id).count)
+      @rankings[team.name]["win_percent"] = (wins / (game_count.nonzero? || 1)).to_f
       # need to update schema before adding ties
       @rankings[team.name]["ties"] = 0
     end
-    # order rank by games won percentage
-    @rankings = @rankings.sort_by{|k,v| v["wins"]}.reverse
+    # order rank by games win percentage
+    @rankings = @rankings.sort_by{|k,v| v["win_percent"]}.reverse
   end
 end
