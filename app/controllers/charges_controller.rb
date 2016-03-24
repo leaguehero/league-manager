251801a1780bd0c4f7ce65383e8d1ee72a631f1e
Rob Schwartz <robertschwar@gmail.com>
2016-03-23 22:13:51 -0500

    commented out unneeded stripe code in charges controller, changed after signup path

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,48 +8,48 @@ class ChargesController < ApplicationController
 
   def create
     # set api key for Stripe calls
-    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    # Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
     pl = PreLeague.find(current_user.pre_league_id)
     # Get the credit card details submitted by the form
-    token = params[:stripeToken]
-    league = pl["league_name"]
-    @amount  = (pl[:max_teams] * pl[:max_players_per_team])
-    @amount_in_cents = @amount * 100
+    # token = params[:stripeToken]
+    # league = pl["league_name"]
+    # @amount  = (pl[:max_teams] * pl[:max_players_per_team])
+    # @amount_in_cents = @amount * 100
     # Create the charge on Stripe's servers - this will charge the user's card
-    begin
-
-      # check for card errors
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
-    end
+    # begin
+    #
+    #   # check for card errors
+    # rescue Stripe::CardError => e
+    #   flash[:error] = e.message
+    #   redirect_to new_charge_path
+    # end
     # Do everything after the payment has been set up
     # create league specific plan
-    begin
-      Stripe::Plan.create(
-        :amount => @amount_in_cents,
-        :interval => "year",
-        :name => pl["league_name"]+ " League Plan" ,
-        :currency => "usd",
-        :id => pl["league_name"],
-        :trial_period_days => 30
-      )
-      # create stripe customer
-      stripe_customer = Stripe::Customer.create(
-      :source => token,
-      :email => current_user.email,
-      :plan => pl["league_name"],
-      :description => "Admin for " + pl["league_name"]
-      )
-      # update current_user with subdomain
-      user = User.find_by_email(current_user.email)
-      # add stripe id to user
-      user.stripe_id = stripe_customer.id
-      user.save!
-    rescue
-      # plan and customer were already created, move on
-    end
+    # begin
+    #   Stripe::Plan.create(
+    #     :amount => @amount_in_cents,
+    #     :interval => "year",
+    #     :name => pl["league_name"]+ " League Plan" ,
+    #     :currency => "usd",
+    #     :id => pl["league_name"],
+    #     :trial_period_days => 30
+    #   )
+    #   # create stripe customer
+    #   stripe_customer = Stripe::Customer.create(
+    #   :source => token,
+    #   :email => current_user.email,
+    #   :plan => pl["league_name"],
+    #   :description => "Admin for " + pl["league_name"]
+    #   )
+    #   # update current_user with subdomain
+    #   user = User.find_by_email(current_user.email)
+    #   # add stripe id to user
+    #   user.stripe_id = stripe_customer.id
+    #   user.save!
+    # rescue
+    #   # plan and customer were already created, move on
+    # end
     # build league if card goes through
     League.create(
       :name => pl["league_name"],
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -36,7 +36,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    "/charges/new"
+    "/charges/confirmation"
   end
 
 end
